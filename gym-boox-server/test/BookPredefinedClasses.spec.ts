1f9c4041500a586df7203bd728ef1a7317590f9a
Joachim Chapman <jc@quack.zone>
2018-04-30 00:36:02 +0100

    progress on mocked version of login

--- gym-boox-server/test/BookPredefinedClasses.spec.ts
+++ gym-boox-server/test/BookPredefinedClasses.spec.ts
@@ -2,16 +2,41 @@ import 'mocha';
 import {expect} from 'chai';
 
 import {bookPreDefinedClasses} from "../handler";
+import * as nock from "nock";
+import {Authentication} from "./gym-box-recordings/authentication";
 
-describe("Given a matching class", () => {
-    it("Then book the class", async function () {
-        const result = await bookPreDefinedClasses();
-        expect(result.statusCode).to.be.equal(200);
+describe("Book Predefined Classes", () => {
+
+    before(() => {
+        nock.disableNetConnect()
+    })
+    after(() => {
+        nock.enableNetConnect()
+    })
+
+    // beforeEach(() => {
+    //  nock.activate();
+    // });
+
+    afterEach(() => {
+        nock.cleanAll();
+        // nock.restore();
     });
-    it("Then don't book the class if it's already been booked")
-});
 
-describe("Given no matching classes", () => {
-    it("Then make no bookings");
+
+    describe("Given a matching class", () => {
+
+        it("Then book the class", async function () {
+            nock.define([Authentication.login, Authentication.logout]);
+            const result = await bookPreDefinedClasses();
+            expect(result.statusCode).to.be.equal(200, result.body);
+        });
+
+        it("Then don't book the class if it's already been booked")
+    });
+
+    describe("Given no matching classes", () => {
+        it("Then make no bookings");
+    });
 });
 
