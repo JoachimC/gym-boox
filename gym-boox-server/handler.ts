aa36d0d42e27a39e76885a1650720571fd8b3bb2
Joachim Chapman <jc@quack.zone>
2018-06-22 17:15:25 +0100

    remove 'hello world' end-point

--- gym-boox-server/handler.ts
+++ gym-boox-server/handler.ts
@@ -1,17 +1,8 @@
-import {Response, Handler} from "aws-lambda";
+import {Handler} from "aws-lambda";
 import {BookPredefinedClassesHttpPort} from "./src/bookPredefinedClasses/BookPredefinedClassesHttpPort";
 import {BookPredefinedClassesCronPort} from "./src/bookPredefinedClasses/BookPredefinedClassesCronPort";
-
-const hello: Handler = async (): Promise<Response> => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: Math.floor(Math.random() * 10)
-        })
-    };
-};
 
 const bookPredefinedClassesHttp: Handler = BookPredefinedClassesHttpPort.handle;
 const bookPredefinedClasses: Handler = BookPredefinedClassesCronPort.handle;
 
-export {hello, bookPredefinedClasses, bookPredefinedClassesHttp}
+export {bookPredefinedClasses, bookPredefinedClassesHttp}
